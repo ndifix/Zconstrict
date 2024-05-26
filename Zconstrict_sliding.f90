04ca6a00f5081ae748670650821cd85e72f86bbb
ndifix <yoshiki.linuxmint@gmail.com>
2024-05-26 15:35:58 +0000

    空行の削除

--- Zconstrict_sliding.f90
+++ Zconstrict_sliding.f90
@@ -97,12 +97,8 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 
    nw=0
    nb=0
-
    xmin=-(nxsol-1)/2*dxsol
-
    xmax=-xmin
-
-
    xboundmin=xmin
    xboundmax=xmax
 
@@ -110,23 +106,14 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 
 !print*,'check'
 
-
    do jx=1,nxsol
-
       x0=xmin+(jx-1)*dxsol
-
       xwall(1:nphisol,jx)=x0
-
       do jp=1,nphisol
-
          nw=nw+1
-
          ywall(jp,jx)=walrad*cos(jp*dphisol)
-
          zwall(jp,jx)=walrad*sin(jp*dphisol)
-
          nb=nb+1
-
          bondwal(1,nb)=nw
 
          if(jp<nphisol)then
@@ -147,13 +134,9 @@ integer omp_get_thread_num,omp_get_num_threads,tid
       end if
 
       do jp=nw-nphisol+1,nw
-
          nb=nb+1
-
          bondwal(1,nb)=jp-nphisol
-
          bondwal(2,nb)=jp
-
       end do
 
    end do
@@ -161,13 +144,11 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    allocate(rwall(nxsol),lwall(nphisol,nxsol))
 
    rwall=walrad
-
    l0=sqrt((ywall(1,1)-ywall(2,1))**2+(zwall(1,1)-zwall(2,1))**2)
 
 !  to make glycan force balancing turgor at the beginning
 
    lwall=l0-fturgor*walrad/kgly/l0
-
    lpep=dxsol
 
 !print*,'check'
@@ -181,7 +162,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 !  if the bead size is l_mem then number of bead per hoop:
 
    nmeho=2*pi*mbrad/l_mem
-
    dphi=2*pi/nmeho
 
 !  number of hoops:
@@ -198,28 +178,15 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 
    allocate(xmemb(nmemb),ymemb(nmemb),zmemb(nmemb))
 
-
    nm=0
-
    do jh=1,nhoop
-
       x0=-(nhoop-1)/2*l_mem+(jh-1)*l_mem !xmin+(jh-1)*l_mem
-
       xmemb(nm+1:nm+nmeho)=x0
-
-
       do jm=1,nmeho
-
          nm=nm+1
-
          ymemb(nm)=mbrad*cos(dphi*jm)
-
          zmemb(nm)=mbrad*sin(dphi*jm)
-
-
-
       end do
-
    end do
 
 !  coarse representation of membrane surface:
@@ -231,7 +198,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 !  but:
 
    ysurf=ywall*mbrad/walrad
-
    zsurf=zwall*mbrad/walrad
 
 !------------------------------------------------------
@@ -241,7 +207,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 
 !   xzmax=rwid/2
 !   xzmin=-xzmax
-
 
 !  the "origin" shifted:
 
@@ -255,7 +220,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
       print*,'ring width RWID =',rwid
       stop
    end if
-
 
 !  number of subuints per a complete ring:
    nsunit=2*pi*rrad/l_z
@@ -273,7 +237,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    nfil=4*nftsz/zlen
 
 !  filament id:
-
    allocate(filid(nftsz))
 
 !  ring id
@@ -287,12 +250,9 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    fil2a=0
 
 !  limit # of FtsA:
-
    nftsamax=nftsz!*p_tether*2
 
-
    allocate(xa(nftsamax),ya(nftsamax),za(nftsamax))
-
    allocate(a2mem(nftsamax),a2fil(nftsamax))
    a2mem=0
    a2fil=0
@@ -300,126 +260,70 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    allocate(mark(nmemb))
    mark=0
 
-
-
 !  filling space with subunits:
 
 !   allocate(fill(nsunit,nring+1))
 !   fill=0
-
-
    nftsa=0
-
 !   nmono=nftsz
-
    npoly=0
-
-
    dphi=2*pi/nsunit
-
 !   rad12=rrad+l_z/2
-
 !   rad34=rad12-l_z
-
 !   fstart0=1
-
 !   length0=0
 
    flen=0
-
-
    nfil=0
-
 
 !-----------------------
 
    do nr=1,nring
-
 !      x0=xorigin+(nr-1)*xgap
-
 !     pick the angle of first bead:
-
       call random_number(r)
-
       phi0=2*pi*r
-
       ncover=0
-
       do n=1,1000
-
          if(ncover==nsunit) exit
-
          nfil=nfil+1
-
          call random_number(r)
-
          length=zlen+(r-0.5)*10
-
          if(nsunit-ncover-length<zlen/2) length=nsunit-ncover
-
          filid(npoly+1:npoly+length+overlap)=nfil
-
          ringid(npoly+1:npoly+length+overlap)=nr
-
          flen(nfil)=length+overlap
-
          fstart(nfil)=npoly+1
-
          dxshift=rho/(length+overlap)
-
          x0=xorigin+(nr-1)*xgap
-
          call random_number(r)
-
          if(r>0.5) then
-
             jstart=ncover+1
-
             jdir=1
-
          else
-
             dxshift=-dxshift
-
             jstart=ncover+length+overlap
-
             x0=x0+rho
-
             jdir=-1
-
          end if
 
 !        now assign coordinates:
 
          do jf=1,length+overlap
-
             npoly=npoly+1
-
             xcen(npoly)=x0
-
             phi=phi0+(jstart+(jf-1)*jdir)*dphi
-
             ycen(npoly)=rrad*cos(phi)
-
             zcen(npoly)=rrad*sin(phi)
 
 !           assign tethering on the beads:
-
             if(mod(jf,2)==1)then
-
-
                jh=(x0-xmin)/l_mem+1
-
-
                jm0=(jh-1)*nmeho
-
                dist2=9*ltether**2
-
                npick=0
 
                do jm=jm0+1,jm0+2*nmeho
-
-
                   if(mark(jm)==1)then
                      cycle
                   end if
@@ -427,17 +331,11 @@ integer omp_get_thread_num,omp_get_num_threads,tid
                   dx=xcen(npoly)-xmemb(jm)
                   dy=ycen(npoly)-ymemb(jm)
                   dz=zcen(npoly)-zmemb(jm)
-
                   d2=dx*dx+dy*dy+dz*dz
-
                   if(d2<dist2)then
-
                      dist2=d2
-
                      npick=jm
-
                   end if
-
                end do
 
                if(npick==0)then
@@ -446,40 +344,24 @@ integer omp_get_thread_num,omp_get_num_threads,tid
                end if
 
                nftsa=nftsa+1
-
                fil2a(npoly)=nftsa
-
                a2mem(nftsa)=npick
-
                a2fil(nftsa)=npoly
-
                mark(npick)=1
-
                xa(nftsa)=0.5d0*(xcen(npoly)+xmemb(npick))
                ya(nftsa)=0.5d0*(ycen(npoly)+ymemb(npick))
                za(nftsa)=0.5d0*(zcen(npoly)+zmemb(npick))
-
-
             end if
 
             x0=x0+dxshift
-
-
-
          end do
 
-
          ncover=ncover+length
-
       end do
-
    end do
 
-
    nftsz=npoly
-
    nftsamax=nftsa
-
 !   nfilmax=nfil
 
 !------------------------------
@@ -489,7 +371,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
                fil2a,fstart,flen,filid,ringid,a2mem,a2fil,bondwal,dxsol,dphisol,xboundmin,xboundmax, &
                 rwall,lwall,xwall,ywall,zwall,xnorwall,ynorwall,znorwall,xsurf,ysurf,zsurf, &
                  xmemb,ymemb,zmemb,xcen,ycen,zcen,xa,ya,za)
-
 
 !  clean up
 
@@ -506,55 +387,35 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 29 call getinfo(nstart,jfile,time0,jstart,runtime,natom,natom_zc, &
          natom_a,natom_z2a,nwall,nxsol,nphisol,nmemb,nftsz,npoly,nfil,zlen,nftsa,nftsamax)
 
-
    if(jstart>nstep) stop
 
    print*,'=================================='
    print*,'ring dynamics'
 
 !  setting parameters:
-
-
    ptread=ptread/1000000 ! unit = inverse micro second
-
    phyd=phyd/1000000 ! unit = inverse micro second
-
    pdepo=pdepo/1000000 ! unit = inverse micro second
 
 !  addition rate of FtsZ filament
-
    padd=1.0d0/tadd/1000000 ! unit = inverse micro second
 
 !  limit of wall remodeling rate
-
    rateremod=rateremod/1000000 ! unit = nm/micro second
 
 !  cut-off distance:
-
    cutoff=rcutoff*rho
-
    rswitch=cutoff-0.5d0*rho
-
    fswitch=6*eps*(2*(rho/rswitch)**12-(rho/rswitch)**6)/rswitch/(cutoff-rswitch)**2
 
-
-
 !  number of adjacent beads to be exclude from interaction:
-
    nxclude=cutoff/l_z+1
 
 !  parameters for attraction between membrane beads:
-
-
    l_pair=2.0d0*l_mem
-
    thetpair=4*l_mem/rrad
-
    cos_t_2=(1.0d0-(2*thetpair)**2/2)**2
-
    thet2by2=thetpair*thetpair/2
-
-
 
    oldtime=runtime
 
@@ -562,37 +423,25 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 !-----------------------------------
 
 !  system configuration:
-
    allocate(rwall(nxsol),lwall(nphisol,nxsol),xwall(nphisol,nxsol),ywall(nphisol,nxsol),zwall(nphisol,nxsol))
    allocate(xnorwall(nphisol,nxsol),ynorwall(nphisol,nxsol),znorwall(nphisol,nxsol))
-
-
    allocate(xmemb(nmemb),ymemb(nmemb),zmemb(nmemb),jmbsol(2,nmemb))
    allocate(xsurf(nphisol,nxsol),ysurf(nphisol,nxsol),zsurf(nphisol,nxsol))
-
    allocate(jsursol(2,nmemb),nsurf(nphisol,nxsol))
    allocate(xnorsurf(nphisol,nxsol),ynorsurf(nphisol,nxsol),znorsurf(nphisol,nxsol))
 
 !  check distance between membrane and cell wall
-
    allocate(jdist(nphisol,nxsol))
 
 !  center of the subunits:
    allocate(xcen(nftsz),ycen(nftsz),zcen(nftsz))
-
-
    allocate(fstart(nfil),flen(nfil),jzsol(2,nftsz),filid(nftsz),ringid(nftsz),gtp(nftsz),endfil(nftsz))
 
 !  FtsA:
-
-
    allocate(xa(nftsamax),ya(nftsamax),za(nftsamax))
 
-
 !  membrane tethering:
-
    allocate(fil2a(nftsz),a2mem(nftsamax),a2fil(nftsamax))
-
 
    call ringin(nstart,nmemb,nfil,npoly,nftsa,nphisol,nxsol, &
                filid,ringid,fil2a,flen,fstart,a2mem,a2fil,endfil,gtp, &
@@ -600,35 +449,23 @@ integer omp_get_thread_num,omp_get_num_threads,tid
                  xsurf,ysurf,zsurf,xnorsurf,ynorsurf,znorsurf,xmemb,ymemb,zmemb, &
                   xcen,ycen,zcen,xa,ya,za)
 
-
-
 !------------------------------------------------------------
 
 !  forces:
    allocate(fywall(nphisol,nxsol),fzwall(nphisol,nxsol))
-
    allocate(fxmemb(nmemb),fymemb(nmemb),fzmemb(nmemb))
-
 !   allocate(fx_z(4,nftsz),fy_z(4,nftsz),fz_z(4,nftsz))
    allocate(fxfil(nftsz),fyfil(nftsz),fzfil(nftsz))
-
    allocate(fxrep(nftsz),fyrep(nftsz),fzrep(nftsz))
-
 !   allocate(fxtem(nthreads,4,nftsz),fytem(nthreads,4,nftsz),fztem(nthreads,4,nftsz))
-
-
    allocate(fxa(nftsamax),fya(nftsamax),fza(nftsamax))
-
-
 
 !  to add random forces:
 
 !OK   nrforce=1028
 
    allocate(rxmemb(nmemb,nrforce),rymemb(nmemb,nrforce),rzmemb(nmemb,nrforce))
-
    allocate(rxbead(nftsz,nrforce),rybead(nftsz,nrforce),rzbead(nftsz,nrforce))
-
    allocate(rx_a(nftsamax,nrforce),ry_a(nftsamax,nrforce),rz_a(nftsamax,nrforce))
 
    jrforce=nrforce
@@ -638,34 +475,26 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 !  preserving membrane stiffness and integrity
 
 !  pairs of membrane beads:
-
    allocate(pair5_mb(2,nmemb*500),pair_mb(2,nmemb*100))
-
    allocate(pairpart(2,nmemb*100),pairtyp(nmemb*100))
 
 !  boundary problem:
-
    allocate(boundtyp(nmemb))
 
 !  for connecting two boundaries:
-
    wallwidth=xboundmax-xboundmin
    shift=wallwidth+l_mem
 
-
 !  pairs of FtsZ beads:
-
    allocate(pair5_z(2,nftsz*500),pair_z(2,nftsz*100))
 
 !------------------------------------------------------------
 
 !  To write out trajectory
-
    call random_number(r)
    junit=80*r+11
    junit1=junit+1
    junit2=junit+2
-
    jfile=jfile+1
    call dcdheader(junit,jfile,natom)
 
@@ -674,28 +503,20 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    call writedcd(junit,nframe,natom,natom_zc,natom_a,natom_z2a,nxsol,nphisol,nmemb,nftsz,nfil,nftsa, &
                 flen,a2mem,a2fil,fstart,xcen,ycen,zcen,xa,ya,za, &
                  xmemb,ymemb,zmemb,xwall,ywall,zwall)
-   
 
 !  initial setup solid angle indices:
-
    call solidset(nxsol,nphisol,nmemb,npoly,jsursol,jmbsol,jzsol,pi,delta, &
               dxsol,dphisol,xmemb,ymemb,zmemb,xcen,ycen,zcen,xwall,ywall,zwall, &
               xnorwall,ynorwall,znorwall,xsurf,ysurf,zsurf,xnorsurf,ynorsurf,znorsurf)
 
 !  setup wall growth gradient:
-
    allocate(gradient(nxsol))
 
    jxwid=(nxsol-1)/2
-
    do jx=1,nxsol
-
       dx=1.0d0*(-jxwid+jx-1)
-
       dx=dx/jxwid
-
       gradient(jx)=exp(-dx*dx*fgrad)
-
    end do
 
 !  measuring constriction rate
@@ -707,30 +528,19 @@ integer omp_get_thread_num,omp_get_num_threads,tid
    open(junit2,file='constrict_rate.txt')
 
    if(runtime<1.0)then
-
       write(junit2,'(a43)')'   time(s)  wall_constrict(nm)  rate (nm/s)'
-
       wallcons=0.0d0
-
    else
-
       length=1
-
       read(junit2,'(a43)')a43
-
       do n=1,10000000
-
          read(junit2,*)timecheck,rwallcons,cons_rate
-
          wallcons=rwallcons
-
          if(timecheck>=timecheck0)then
             exit
          end if
 
          length=length+1
-
-
       end do
 
       close(junit2)
@@ -740,114 +550,74 @@ integer omp_get_thread_num,omp_get_num_threads,tid
       do n=1,length
          read(junit2,*)
       end do
-
-
    end if
 
    oldwallcons=wallcons
 
    ndist=1000 ! just to prevent cell wall remodeling from occurring too early
-
    jdist=0
-
    nskip1=10
    nskip2=10000
 !=============================================================
 
-
 !  Dynamics of the ring:
-
    print*,'start dynamics'
-
    write(*,*)'timestep     runtime     '
 
 !  start time step:
 !   dt=0.0d0
-
    dtmodz=0.0d0
-
    dtremod=0.0d0
-
    do jstep=jstart,nstep
-
       if(mod(jstep-1,nskip1)==0.or.jstep==jstart)then
          jforce=1
       else
          jforce=0
       end if
 
-
 !     set random force
-
-
       if(jrforce==nrforce) &
          call rforceset(jrforce,nrforce,nmemb,nftsz,nftsamax,pi,rxmemb,rymemb,rzmemb, &
               rxbead,rybead,rzbead,rx_a,ry_a,rz_a,k_scale,k_scale_mb)
 
-
-
       if(mod(jstep-1,nskip2)==0.or.jstep==jstart)then
-
          if(mod(jstep-1,nturnover)==0.or.jstep==jstart)then
-
 !           treadmilling of FtsZ
-
             if(jtreadmil==1) then
                call treadmill(nfil,nftsa,nmemb,fstart,flen,fil2a,a2fil,a2mem,gtp,jzsol, &
                      ptread,phyd,dtmodz,xcen,ycen,zcen,xa,ya,za,xmemb,ymemb,zmemb)
 
                dtmodz=0.0d0
-
             elseif(jdepo==1)then
-
                call depoly(nfil,npoly,nftsa,flen,fstart,filid,fil2a,a2mem,a2fil,endfil,ringid, &
                     jzsol,pdepo,dtmodz,xcen,ycen,zcen,xa,ya,za)
 
-
                dtmodz=0.0d0
-
             end if
 
-
 !              setup pairs for long run:
-
             call allpairs(npoly,nmemb,nxclude,intering,npair5_z,npair5_mb,filid,ringid, &
                pair5_z,pair5_mb,cutoff,cos_t_2,xcen,ycen,zcen,xmemb,ymemb,zmemb)
-
-
-
          end if
 
-
-
 !           categorize beads into solid angles:
-
          call solidupdate(nxsol,nphisol,nmemb,npoly,nftsa,jmbsol,jsursol,jzsol,a2mem, &
               pi,delta,dxsol,dphisol,xmemb,ymemb,zmemb,xcen,ycen,zcen,xwall,ywall,zwall, &
               xnorwall,ynorwall,znorwall,xsurf,ysurf,zsurf,xnorsurf,ynorsurf,znorsurf)
 
-
 !           pair setup for exclusion effect:
-
          call setpair(nmemb,npair5_z,npair5_mb,npair_z,npair_mb,pair5_z,pair5_mb, &
               pair_z,pair_mb,cutoff,l_pair,thet2by2,xcen,ycen,zcen,xmemb,ymemb,zmemb, &
               pairpart,boundtyp,pairtyp,l_mem,xboundmin,xboundmax,shift)
 
-
-
-
-
          call surfremod(nmemb,nthreads,nphisol,nxsol,nwall,jgradient,jnogradient,ndist,jsursol,jmbsol,nsurf,jdist, &
                wthick,gap,growstep,dphisol,wallwidth,pi,wallcons,xmemb,ymemb,zmemb,xsurf,ysurf,zsurf,xnorsurf, &
                 ynorsurf,znorsurf,xwall,ywall,zwall,xnorwall,ynorwall,znorwall,rwall,lwall,gradient,dtremod,rateremod)
-
       end if
-
 
 !----------------------------------------------------------------
 
 !     calculate forces:
-
       if(jforce==1)then
 
          jrforce=jrforce+1
@@ -855,7 +625,6 @@ integer omp_get_thread_num,omp_get_num_threads,tid
          fxmemb(1:nmemb)=rxmemb(1:nmemb,jrforce)
          fymemb(1:nmemb)=rymemb(1:nmemb,jrforce)
          fzmemb(1:nmemb)=rzmemb(1:nmemb,jrforce)
-
 
          fxa(1:nftsa)=rx_a(1:nftsa,jrforce)
          fya(1:nftsa)=ry_a(1:nftsa,jrforce)
@@ -865,10 +634,7 @@ integer omp_get_thread_num,omp_get_num_threads,tid
          fyrep(1:npoly)=rybead(1:npoly,jrforce)
          fzrep(1:npoly)=rzbead(1:npoly,jrforce)
 
-
-
 !        constraint forces: actin-membrane tether, wall blocking, boundaries
-
          call constraints(nthreads,nmemb,npoly,nftsa,nxsol,nphisol,jmbsol,jsursol,jzsol,a2mem,a2fil,fil2a,endfil,nsurf, &
                kwall,wthick,lsqueez,l_mem,k_mem,l_a_z,l_mb_a,ktether, &
                 xwall,ywall,zwall,xnorwall,ynorwall,znorwall,xsurf,ysurf,zsurf, &
@@ -876,116 +642,70 @@ integer omp_get_thread_num,omp_get_num_threads,tid
                   xcen,ycen,zcen,xa,ya,za,fxmemb,fymemb,fzmemb, &
                    fxrep,fyrep,fzrep,fxa,fya,fza)
 
-
 !        interaction among membrane beads
-
          call membrane(npair_mb,pair_mb,pairpart,pairtyp,boundtyp,kpair,l_pair,l_mem, &
               kmemb,shift,xmemb,ymemb,zmemb,fxmemb,fymemb,fzmemb)
 
-
 !        cell wall forces
-
          call wallforce(nxsol,nphisol,kgly,kpep,dxsol,pturgor,delta,invdelta,lwall,xwall,ywall,zwall,fywall,fzwall)
 
-
 !        FtsZ bending
-
          call zangle(nfil,fstart,flen,beta,delta,invdelta,kz_thet,thet0_z,xcen,ycen,zcen,fxrep,fyrep,fzrep)
 
 !        calculate distance between membrane and cell wall
-
          call checkdistance(nskip1,nskip2,nmemb,nphisol,nxsol,jmbsol,ndist,jdist,wthick,gap, &
                             xmemb,ymemb,zmemb,xwall,ywall,zwall,xnorwall,ynorwall,znorwall)
-
-
-
       end if
 
 !     rigidity of FtsZ and FtsA:
-
       fxfil(1:npoly)=fxrep(1:npoly)
       fyfil(1:npoly)=fyrep(1:npoly)
       fzfil(1:npoly)=fzrep(1:npoly)
 
-
-
-
       call zrigid(nfil,npair_z,fstart,flen,pair_z,k_z,l_z,cutoff,rho,eps,rswitch,fswitch, &
            xcen,ycen,zcen,fxfil,fyfil,fzfil)
-
-
-
-
 
 !----------------------------------------------------
 
 !     update coordinates:
-
       if(jforce==1)then
-
          fmaxmb2=maxval(fxmemb*fxmemb+fymemb*fymemb+fzmemb*fzmemb)
-
       end if
-
-
-
 
       call newcoor(npoly,nftsa,nmemb,fmaxmb2,viscos,dtmodz,dtremod,runtime,fxmemb,fymemb,fzmemb, &
                fxa,fya,fza,fxfil,fyfil,fzfil,xmemb,ymemb,zmemb,xa,ya,za,xcen,ycen,zcen, &
                 jforce,nxsol,nphisol,wallwidth,fywall,fzwall,xwall,ywall,zwall,xnorwall,ynorwall,znorwall)
 
-
-
 !----------------------------------------------------
-
 
       if(runtime-oldtime>printtime)then
 
 !      if(mod(jstep-1,1)==0)then
-
          dtime=dtime+runtime-oldtime
-
          oldtime=runtime
 
          write(*,16)jstep,runtime*0.000001,'sec'
-
          call writedcd(junit,nframe,natom,natom_zc,natom_a,natom_z2a,nxsol,nphisol,nmemb,nftsz,nfil,nftsa, &
                 flen,a2mem,a2fil,fstart,xcen,ycen,zcen,xa,ya,za, &
                  xmemb,ymemb,zmemb,xwall,ywall,zwall)
 
-
          if(mod(nframe,5)==0)then
-
             dtime=dtime*0.000001
-
             cons_rate=(wallcons-oldwallcons)/dtime
-
             rwallcons=wallcons
-
             rtime=runtime*0.000001
-
             write(junit2,*)rtime,rwallcons,cons_rate
-
             oldwallcons=wallcons
-
             dtime=0.0d0
-
          end if
 
-
          if(nframe>=noutput)then
-
             close(junit)
-
             call ringout(nstart,natom,natom_zc,natom_a,natom_z2a,nwall,nxsol,nphisol,nmemb,nftsz,npoly,nfil, &
                zlen,nftsa,nftsamax,fstart,flen,filid,ringid,fil2a,gtp,a2mem,a2fil, &
                 dxsol,dphisol,xboundmin,xboundmax,rwall,lwall,xwall,ywall,zwall,xnorwall,ynorwall,znorwall, &
                  xnorsurf,ynorsurf,znorsurf,xsurf,ysurf,zsurf,xmemb,ymemb,zmemb,xcen,ycen,zcen,xa,ya,za)
-
-
-
             call system_clock(timerun)
-
 
             OPEN(junit1,FILE='restart.inp')
             write(junit1,'(a65)')'      nstart       jfile       jstart        time0        runtime'
@@ -993,21 +713,15 @@ integer omp_get_thread_num,omp_get_num_threads,tid
             WRITE(junit1,*)NSTART,JFILE,jstep+1,(TIMERUN-TIMESTART)/rate+time0,runtime
             CLOSE(junit1)
 
-
 !            if((nstep-jstep)/jprint>0)then
                jfile=jfile+1
                call dcdheader(junit,jfile,natom)
                nframe=0
 !            end if
-
          end if
-
       end if
-
-
    end do
 !------------------------------
-
 
    call system_clock(timerun)
 
@@ -1026,19 +740,7 @@ integer omp_get_thread_num,omp_get_num_threads,tid
 
 1  FORMAT(A14,2X,I2,1X,A7,I2,1X,A8,1X,I2,1X,A7,1x,I2,1x,A4)
 
-
-
 16 format(i16,2x,f10.5,1x,a3)
-
-
-
 100 print*,'end of simulation'
-
-
-
 end
-
-
-
-
 
